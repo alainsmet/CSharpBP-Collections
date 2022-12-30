992ed5381bf33abdcf3cd52c39abe285f276a778
Alain Smet <arpasmet@gmail.com>
2022-12-30 20:48:49 +0200

    Completion of module 3 - Arrays

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,6 +16,32 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
+            string[] colorOptions = { "Red", "Espresso", "White", "Navy" };
+
+            //var colorOptions = new string[4] { "Red", "Espresso", "White", "Navy" };
+
+            //var colorOptions = new string[4];
+            //colorOptions[0] = "Red";
+            //colorOptions[1] = "Espresso";
+            //colorOptions[2] = "White";
+            //colorOptions[3] = "Navy";
+
+            //Console.WriteLine(colorOptions[1]);
+
+            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+            
+            colorOptions.SetValue("Blue", 3);
+
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+
+            foreach (string color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
+            
         }
         public Product(int productId,
                         string productName,
